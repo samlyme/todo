c369f3d59ea39d3cf518ef0065fd05c53d269c2b
Sam Ly <sam@samly.me>
2025-03-23 04:59:02 -0700

    trim

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI, HTTPException
-
-from database import create_tables, drop_tables
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -15,16 +13,3 @@ async def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
-
-
-@app.post('/initdb')
-async def initdb():
-    try:
-        drop_tables()
-        create_tables()
-        return {"message": "Tables dropped"}
-    except Exception as e:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Error {e}"
-        )
